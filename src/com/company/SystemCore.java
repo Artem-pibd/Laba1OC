5a03e665a8d369972e3a0b5b4032e75186f8d0b2
Artem-pibd <dyrnochkinartem@mail.ru>
2020-12-09 14:17:24 +0400

    update

--- src/com/company/SystemCore.java
+++ src/com/company/SystemCore.java
@@ -1,43 +1,44 @@
 package com.company;
 
+import java.util.HashMap;
 
 public class SystemCore {
 
-    private final int count = 5;
-    private  SystemCall[] systemCalls = new SystemCall[count];
+    private final int count = 6;
+    private HashMap<Integer,SystemCall> systemCalls;
     private Stack stack;
 
-    public SystemCore(Stack stack) {
+    public SystemCore(Stack stack ) {
         this.stack = stack;
+        systemCalls = new HashMap<>();
 
-        systemCalls [0] = new SystemCall(0 ,"Integer String");
-        systemCalls [1] = new SystemCall(1 ,"String Integer String");
-        systemCalls [2] = new SystemCall(2 ,"Double Double");
-        systemCalls [3] = new SystemCall(3 ,"Double Double String");
-        systemCalls [4] = new SystemCall(4 ,"Double String");
-
+        systemCalls.put(1,new SystemCall(1 ,"Integer Integer"));
+        systemCalls.put(2,new SystemCall(2 ,"String Integer String"));
+        systemCalls.put(3,new SystemCall(3 ,"Double Double"));
+        systemCalls.put(4,new SystemCall(4 ,"Double Double String"));
+        systemCalls.put(5,new SystemCall(5 ,"Double String"));
     }
 
     public void print () {
-        for (int i = 0; i < count; i++) {
-            System.out.println(" Id системного вызова: " + systemCalls[i].getIndex() + ". Параметры:  " + systemCalls[i].getParameters() );
+        for (int i = 1; i < count; i++) {
+            System.out.println(" Id системного вызова: " + systemCalls.get(i).getIndex() + ". Параметры:  " + systemCalls.get(i).getParameters());
         }
     }
 
 
     public  void systemCall (int id) {
 
-        if(id > count || id < 0){
+        if(!systemCalls.containsKey(id)){
             System.out.println("Вызова не существует");
             return;
         }
-        for (int i = 0; i < systemCalls[id].getParameters().split(" ").length; i++) {
-            if(!stack.pop().getClass().getSimpleName().equals(systemCalls[id].getParameters().split(" ")[i])){
+        for (int i = 0; i < systemCalls.get(id).getParameters().split(" ").length; i++) {
+            if(!stack.pop().getClass().getSimpleName().equals(systemCalls.get(id).getParameters().split(" ")[i]) ){
                 System.out.println("Неверный набор аргументов");
                 return;
             }
         }
-        systemCalls[id].call();
+        systemCalls.get(id).call();
     }
 
 }
